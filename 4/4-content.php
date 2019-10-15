bc878bd5469ef1bd7335afb087bac83cd6e9d51a
buixuantu <buixuantu@users.noreply.github.com>
2019-10-15 14:02:48 +0700

    4-module

---
+++ 4/4-content.php
@@ -0,0 +1,61 @@
+  <?php
+  $url_host = 'http://' . $_SERVER['HTTP_HOST'];
+  $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+  $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+  preg_match_all($pattern_uri, __DIR__, $matches);
+  $url_path = $url_host . $matches[1][0];
+  $url_path = str_replace('\\', '/', $url_path);
+  ?>
+
+  <div class="type-4">
+    <div class="conatainer">
+      <div class="row">
+
+        <div class="parallax">
+         <img class="hinh" src="./images/h3-parallax-1.jpg" alt="">
+         <div class="vc_col-sm-12">
+           <figure class="wrap">
+            <img src="./images/h3-img-1.png" alt="">
+          </figure>
+        </div>
+      </div>
+      <div class="vc_row-fluid">
+        <div class="vc_custom">
+          <div class="wp_wrap">
+            <h2 class="title"> what they’re saying </h2>
+          </div>
+          <div class="swiper-container">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget <br>
+               euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur <br> ridiculus mus. Vestibulum ultricies aliquam resto pesct eres furis aviatis.
+               <div class="athur">
+                <h5>William Jones</h5>
+                <p>Designer</p>
+              </div>  
+
+            </div>
+            <div class="swiper-slide">Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut <br>
+             metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.<br> Curabitur ullamcorper ultricies nisi nam eget eres furis aviatis.
+             <div class="athur">
+              <h5> Jennifer Jacobs</h5>
+              <p>Designer</p>
+            </div>  
+          </div>
+
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+        <!-- Add Arrows -->
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+
+
+
---
+++ 4/js/4.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 60,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+});
+});
