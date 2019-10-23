f75af8fe40adc58e8a5ac028cb13ec07e5392f13
buixuantu <buixuantu@users.noreply.github.com>
2019-10-23 07:38:01 +0700

    update

--- 4/4-content.php
+++ 4/4-content.php
@@ -24,12 +24,12 @@
             <div class="col-md-6 col-lg-6 col-sm-6">
               <img class="hinh" src="./images/h3-img-2.jpg" alt="">
             </div>
-           
-              <div class="col-md-6 col-lg-6 col-sm-6">
-                <div class="product">
+
+            <div class="col-md-6 col-lg-6 col-sm-6">
+              <div class="product">
                 <div class="text">
                   <div class="price">
-                    <div class="text-left">
+                    <div class="left">
                       <div class="dola">
                         <div class="currency">
                           $
@@ -37,7 +37,7 @@
                         35
                       </div>
                     </div>
-                    <div class="text-right">
+                    <div class="right">
                       <h5 class="brown">
                         <a href="#">Brown T-Shirt</a>
                       </h5>
@@ -46,55 +46,62 @@
                   </div>
                 </div>
                 <div class="items">
-                  <div class="icon-list">
-                    <span>  <i class="fa fa-check" style="font-size:18px"></i> </span>
+                  <div class="holder">
+                    <div class="icon-list">
+                      <span>  <i class="fa fa-check" style="font-size:18px"></i> </span>
+                    </div>
+                    <p class="lorem">Aliquam lorem ante, dapibus in</p>
                   </div>
-                  <p>Aliquam lorem ante, dapibus in</p>
-                  <div class="icon-list">
-                    <span>  <i class="fa fa-check" style="font-size:18px"></i> </span>
+                  <div class="holder">
+                    <div class="icon-list">
+                      <span>  <i class="fa fa-check" style="font-size:18px"></i> </span>
+                    </div>
+
+                    <p class="lorem">Etiam rhoncus maecenas tempus</p>
                   </div>
-                  <p>Etiam rhoncus maecenas tempus</p>
-                  <div class="icon-list">
-                    <span>  <i class="fa fa-check" style="font-size:18px"></i> </span>
+                  <div class="holder">
+                    <div class="icon-list">
+                      <span>  <i class="fa fa-check" style="font-size:18px"></i> </span>
+                    </div>
+                    <p class="lorem">Lorem ipsum meconsect</p>
                   </div>
-                  <p>Lorem ipsum meconsect</p>
                 </div>
-              </div>
               </div>
             </div>
           </div>
-        </div>      
-        <div class="vc_row-fluid">
-          <div class="vc_custom">
-            <div class="wp_wrap">
-              <h2 class="title"> what they’re saying </h2>
-            </div>
-            <div class="swiper-container">
-              <div class="swiper-wrapper">
-                <div class="swiper-slide">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget <br>
-                 euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur <br> ridiculus mus. Vestibulum ultricies aliquam resto pesct eres furis aviatis.
-                 <div class="athur">
-                  <h5>William Jones</h5>
-                  <p>Designer</p>
-                </div>  
-              </div>
-              <div class="swiper-slide">Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut <br>
-               metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.<br> Curabitur ullamcorper ultricies nisi nam eget eres furis aviatis.
+        </div>   
+      </div>   
+      <div class="vc_row-fluid">
+        <div class="vc_custom">
+          <div class="wp_wrap">
+            <h2 class="title"> what they’re saying </h2>
+          </div>
+          <div class="swiper-container">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget <br>
+               euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur <br> ridiculus mus. Vestibulum ultricies aliquam resto pesct eres furis aviatis.
                <div class="athur">
-                <h5> Jennifer Jacobs</h5>
+                <h5>William Jones</h5>
                 <p>Designer</p>
               </div>  
             </div>
+            <div class="swiper-slide">Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut <br>
+             metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.<br> Curabitur ullamcorper ultricies nisi nam eget eres furis aviatis.
+             <div class="athur">
+              <h5> Jennifer Jacobs</h5>
+              <p>Designer</p>
+            </div>  
           </div>
-          <!-- Add Pagination -->
-          <div class="swiper-pagination"></div>
-          <!-- Add Arrows -->
-          <div class="swiper-button-next"></div>
-          <div class="swiper-button-prev"></div>
         </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+        <!-- Add Arrows -->
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
       </div>
     </div>
   </div>
+</div>
 </div>
 </div>
 
